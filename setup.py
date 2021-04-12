82f4970d21df666b9b4939c7dc7405e85a605283
leaemilie <lea.dorne@yahoo.fr>
2021-04-11 21:34:49 -0400

    Changed setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='omsi',
       url='http://openmsi.nersc.gov',
       packages=['omsi' , 'omsi/analysis' , 'omsi/dataformat' , 'omsi/tools' , 'omsi/viewer'],
       requires=['numpy' , 'h5py'],
-      extras_require=['pillow', 'django', 'psutil', 'pyteomics', 'mpi4py', 'memory_profiler', 'lxml' ],
       license="See licence.txt.",
       keywords="storage MSI analysis workflow provenance"
      )
